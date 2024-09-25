5b6bf7692cbaea9795bd4ffe9d0fad96cdda12b2
SantiagoMartinez0626 <santiago26lopez06@gmail.com>
2024-09-24 19:02:44 -0500

    Functional Test

---
+++ code/migrations/Version20240924235754.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240924235754 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE participation_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE research_project_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE student_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE participation (id INT NOT NULL, student_id INT NOT NULL, research_project_id INT NOT NULL, start_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, estimated_end_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, actual_end_date TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_AB55E24FCB944F1A ON participation (student_id)');
+        $this->addSql('CREATE INDEX IDX_AB55E24FD27719F4 ON participation (research_project_id)');
+        $this->addSql('CREATE TABLE research_project (id INT NOT NULL, name VARCHAR(255) NOT NULL, research_code VARCHAR(255) NOT NULL, available_spots INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B61D60F422A50048 ON research_project (research_code)');
+        $this->addSql('CREATE TABLE student (id INT NOT NULL, full_name VARCHAR(255) NOT NULL, university VARCHAR(255) NOT NULL, enrollment_number VARCHAR(255) NOT NULL, enrollment_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B723AF33781FD523 ON student (enrollment_number)');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE participation ADD CONSTRAINT FK_AB55E24FCB944F1A FOREIGN KEY (student_id) REFERENCES student (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE participation ADD CONSTRAINT FK_AB55E24FD27719F4 FOREIGN KEY (research_project_id) REFERENCES research_project (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE participation_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE research_project_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE student_id_seq CASCADE');
+        $this->addSql('ALTER TABLE participation DROP CONSTRAINT FK_AB55E24FCB944F1A');
+        $this->addSql('ALTER TABLE participation DROP CONSTRAINT FK_AB55E24FD27719F4');
+        $this->addSql('DROP TABLE participation');
+        $this->addSql('DROP TABLE research_project');
+        $this->addSql('DROP TABLE student');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
